0b35d58386ca49a716d70830c23119f1c1cd953f
Arrancara <87420856+Arrancara@users.noreply.github.com>
2021-08-12 10:23:40 +0100

    Additional files.
    Additional classes and files.

---
+++ Arc.py
@@ -0,0 +1,8 @@
+class Arc():
+    def __init__(self, x: float, y:float, angleinitial: float, anglefinal : float, radius :float):
+        self.x = x
+        self.y = y
+        self.angleinitial = angleinitial
+        self.anglefinal = anglefinal
+        self.radius = radius
+
---
+++ rectangle.py
@@ -0,0 +1,16 @@
+class Rectangle():
+    def __init__(self, x: float, y:float, width: float, height: float):
+        # Give attributes here that you want it to have when you create an isntance of it.
+
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+      ## x = the left most point or Point2 , y value of the right most point.
+
+    ##now we calculate the points and where they are,
+
+
+
+
+
